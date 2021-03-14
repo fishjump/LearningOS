9b4d0f7c6bece1a1601f67a29d6e69f405bec2c1
fishjump <z1052525203@outlook.com>
2021-03-14 23:37:50 +0800

    mv keyboard to module

--- src/kernel/include/system.hpp
+++ src/kernel/include/system.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <boot/boot.hpp>
+#include <color/color.hpp>
 #include <global/global.hpp>
 #include <interrupt/interrupt.hpp>
+#include <keyboard/keyboard.hpp>
 #include <memory/memory.hpp>
-
-#include "system/io.hpp"
-#include "system/media.hpp"
+#include <port/port.hpp>
+#include <screen/screen.hpp>
+#include <text_screen/text_screen.hpp>
--- src/kernel/include/system/io.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <port/port.hpp>
-#include <screen/screen.hpp>
-
-#include "io/keyboard.hpp"
-// #include "io/text_mode_screen.hpp"
